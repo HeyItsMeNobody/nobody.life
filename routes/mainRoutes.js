e4d044d668052487b56e22a646dcdaf20614a457
HeyItsMeNobody <blomdyon@yahoo.com>
2018-11-11 18:32:47 +0100

    Added so that projects gets loaded from MYSQL

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -1,8 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var config = require('../config.json');
+var mysql =  require('mysql');
+var conn = mysql.createConnection({
+    host: config.host,
+    user: config.user,
+    password: config.password,
+    database: config.database
+});
+conn.connect();
+
+//INSERT INTO projects_html (html) VALUES ('<div class="card"><div class="card-header">nobody.life</div><div class="card-main"><div class="card-main-description">The website you are on right now that i use to share most of my projects.<br/>Links:<br/><a href="https://github.com/HeyItsMeNobody/nobody.life">Github</a></div></div></div><br/>')
 
 router.get('/', function (req, res) {
-    res.render('pages/homepage');
+    conn.query('SELECT * FROM `projects_html`', function (error, results, fields) {
+        if (error) throw error;
+        res.render('pages/homepage', { projects: results });
+    });
 });
 
 module.exports = router;
